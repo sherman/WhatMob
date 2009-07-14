3d5e6786762413f2ffc5fb4df25394acf6c21240
Denis M. Gabaidulin <gabaden@gmail.com>
2009-07-14 23:40:28 +0400

    - debug code

--- main.cpp
+++ main.cpp
@@ -32,39 +32,6 @@ int main()
 	if (t) {
 		std::cout << const_cast<object*>(t->getObject())->getValue();
 	}
-
-	/*BinaryTree<std::string> tree;
-
-	std::string s1("nokia");
-	std::string s2("microsotf");
-	std::string s3("asus");
-	std::string s4("Nokia");
-
-	tree.insert(s1);
-	tree.insert(s2);
-	tree.insert(s3);
-	tree.insert(s4);
-
-	std::string s5("Nokia");
-
-	std::string *s = tree.search(s5);
-
-	if (s)
-		std::cout << *s;
-	//if (s)
-
-
 	
-	/*tree.insert('2');
-	tree.insert('3');
-	tree.insert('5');*/
-
-	//tree.traverseInOrder();
-
-	//tree.traversePreOrder();
-	/*SuffixTree tree("nok_Nok_NOk_asus_Nokia3_nokia");
-
-	std::cout << tree.findSubstring("as");*/
-	
-	return 1;
+	return 0;
 }
